65b0a0a4f89e4cc79b83708eccb59cbf1348c808
mliev <server@mliev.com>
2025-08-25 16:33:20 +0800

    feat(config): 从环境变量中读取应用名称

--- config/autoload/base.go
+++ config/autoload/base.go
@@ -7,6 +7,6 @@ type Base struct {
 
 func (receiver Base) InitConfig(helper envInterface.GetHelperInterface) map[string]any {
 	return map[string]any{
-		"app.base.app_name": "go-web-app",
+		"app.base.app_name": helper.GetEnv().GetString("app.base.app_name", "go-web-app"),
 	}
 }
